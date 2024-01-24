3392f789b488c62e6d6d27fc24e4998a168afd33
axdevil <deb.devel@outlook.es>
2024-01-24 00:40:08 -0600

    Cambio de ruta del nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationComponent } from './components/general/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
