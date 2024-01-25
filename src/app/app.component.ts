c42d71e42901210b5366dc413499251eac30b17d
axdevil <deb.devel@outlook.es>
2024-01-24 23:21:33 -0600

    Incorporacion de Aboutme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './components/general/navigation/navigation.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, NavigationComponent, AboutmeComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent {}
