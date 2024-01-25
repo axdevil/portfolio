356a285048dd22737d590dff9a2c6bc00cc74ed9
axdevil <deb.devel@outlook.es>
2024-01-25 12:42:42 -0600

    Cambio de orientacion de navegacion

--- src/app/components/general/navigation/navigation.component.ts
+++ src/app/components/general/navigation/navigation.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'navigation',
-  standalone: true,
-  imports: [],
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+    selector: 'navigation',
+    standalone: true,
+    imports: [],
+    templateUrl: './navigation.component.html',
+    styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
+    @Input() numSection = 0;
 
+    changeClass(): string {
+        if (this.numSection === 0) {
+            return 'bottom';
+        } else {
+            return 'left';
+        }
+    }
+
+    @Output() changeSectionEvent = new EventEmitter<number>();
+    changeSection(section: number) {
+        this.changeSectionEvent.emit(section);
+    }
 }
