b221bc32f3e163173afaf16453f579e51394a2ca
axdevil <deb.devel@outlook.es>
2024-01-25 11:16:02 -0600

    Logica para cambio de section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,31 @@ import { AboutmeComponent } from './components/aboutme/aboutme.component';
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+    numSection = 0;
+
+    scrollDelay = 0;
+    scroll(event: WheelEvent) {
+        const scrollMax = 2;
+
+        if (Math.sign(event.deltaY) === -1) {
+            this.scrollDelay -= 1;
+        } else {
+            this.scrollDelay += 1;
+        }
+
+        if (this.scrollDelay === scrollMax || this.scrollDelay === -scrollMax) {
+            this.changeSection(Math.sign(this.scrollDelay));
+            this.scrollDelay = 0;
+        }
+    }
+
+    changeSection(step: number) {
+        this.numSection += step;
+        if (this.numSection < 0) {
+            this.numSection = 0;
+        } else if (this.numSection > 3) {
+            this.numSection = 3;
+        }
+    }
+}
