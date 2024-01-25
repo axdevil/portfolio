e3fa2187d46365f92101e8908aadd4453f2d9552
axdevil <deb.devel@outlook.es>
2024-01-24 23:20:03 -0600

    Component About me

---
+++ src/app/components/aboutme/aboutme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aboutme',
+  standalone: true,
+  imports: [],
+  templateUrl: './aboutme.component.html',
+  styleUrl: './aboutme.component.scss'
+})
+export class AboutmeComponent {
+
+}
