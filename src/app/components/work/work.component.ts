fd8f4040ac3dfe32c2448d505d09f54900be84c5
axdevil <deb.devel@outlook.es>
2024-01-29 00:01:37 -0600

    Uso de tarjeta

--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { WorkCardComponent } from './work-card/work-card.component';
 
 @Component({
     selector: 'work',
     standalone: true,
-    imports: [],
+    imports: [WorkCardComponent],
     templateUrl: './work.component.html',
     styleUrl: './work.component.scss',
 })
