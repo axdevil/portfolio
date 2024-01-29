efb690af25834eabeaccf7b86952c8f05da9f3e4
axdevil <deb.devel@outlook.es>
2024-01-28 23:09:09 -0600

    Componente de trabajos

---
+++ src/app/components/work/work.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'work',
+    standalone: true,
+    imports: [],
+    templateUrl: './work.component.html',
+    styleUrl: './work.component.scss',
+})
+export class WorkComponent {}
