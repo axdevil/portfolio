3ff7e73b3d017e374dc5dc1b1fdb71b5bc1977d2
axdevil <deb.devel@outlook.es>
2024-01-31 11:40:55 -0600

    Renovacion de tarjeta de trabajos

--- src/app/components/work/work-card/work-card.component.ts
+++ src/app/components/work/work-card/work-card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'work-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './work-card.component.html',
-  styleUrl: './work-card.component.scss'
+    selector: 'work-card',
+    standalone: true,
+    imports: [],
+    templateUrl: './work-card.component.html',
+    styleUrl: './work-card.component.scss',
 })
 export class WorkCardComponent {
-
+    @Input() image!: string;
+    @Input() description!: string;
+    @Input() tags!: [string];
+    @Input() webpage!: string;
 }
