6209d1d7380d4799bc98b9b50c23fee668f4120e
axdevil <deb.devel@outlook.es>
2024-01-31 12:59:47 -0600

    Componente achiviements

---
+++ src/app/components/achiviements/achiviements.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'achiviements',
+    standalone: true,
+    imports: [],
+    templateUrl: './achiviements.component.html',
+    styleUrl: './achiviements.component.scss',
+})
+export class AchiviementsComponent {}
