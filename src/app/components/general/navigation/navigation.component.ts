6fda0157e67580729366c806ba3e04b5c2b900f2
axdevil <deb.devel@outlook.es>
2024-02-08 11:49:37 -0600

    Muestra de contactme

--- src/app/components/general/navigation/navigation.component.ts
+++ src/app/components/general/navigation/navigation.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
     selector: 'navigation',
     standalone: true,
@@ -8,10 +8,32 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
+    constructor(private clipboard: Clipboard) {}
+
     @Input() numSection = 0;
 
     @Output() changeSectionEvent = new EventEmitter<number>();
     changeSection(section: number) {
         this.changeSectionEvent.emit(section);
+    }
+
+    visibleContacts = false;
+
+    toggleContactMe() {
+        if (!this.visibleContacts) {
+            this.visibleContacts = true;
+        } else {
+            this.visibleContacts = false;
+        }
+    }
+
+    emailCopied = false;
+    copyEmail() {
+        this.clipboard.copy('jorgegarciadev@outlook.com');
+        this.emailCopied = true;
+
+        setTimeout(() => {
+            this.emailCopied = false;
+        }, 5000);
     }
 }
